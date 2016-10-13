b79ac0f918e7c36cce3b668add80d32026869df0
mavalver <mvalver@gmail.com>
2016-10-13 23:44:42 +0200

    Initial Commit

---
+++ Entregas Curso Swift iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Entregas Curso Swift iOS
+//
+//  Created by Miguel Angel Garcia Valverde on 13/10/2016.
+//  Copyright © 2016 Miguel Angel Garcia Valverde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
